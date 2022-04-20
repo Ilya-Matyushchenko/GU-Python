d293e1db5cb1eeb2844df28d5cb429fed4048ee9
ilya-matyushchenko <84milyaa@gmail.com>
2022-04-20 19:44:47 +0300

    lesson_1 homework task _3_1

---
+++ Matyushchenko_Ilya_dz_1/task_3_1.py
@@ -0,0 +1,11 @@
+numbers = (11, 12, 13, 14)
+for i in range(100):
+    i = i + 1
+    if i in numbers:
+        print(i, "процентов")
+    elif i % 10 == 1:
+        print(i, "процент")
+    elif (i % 10 > 1) and (i % 10 < 5):
+        print(i, 'процента')
+    else:
+        print(i, 'процентов')
