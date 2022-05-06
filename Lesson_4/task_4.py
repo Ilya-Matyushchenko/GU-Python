c2858d48dc60cd6f2b602eca322161217316ed00
ilya-matyushchenko <84milyaa@gmail.com>
2022-05-06 15:35:54 +0300

    lesson_4 homework

---
+++ Lesson_4/task_4.py
@@ -0,0 +1,9 @@
+from home_work_2_3 import currency_rates
+
+print('Решение задачи №4')
+
+currency_rates(input('Введите название интересующей валюты (usd, rub и т.д.): '))
+
+currency_rates(input('Введите название интересующей валюты (usd, rub и т.д.): '))
+
+currency_rates(input('Введите название интересующей валюты (usd, rub и т.д.): '))
---
+++ Lesson_4/task_5.py
@@ -0,0 +1,6 @@
+from home_work_2_3 import currency_rates
+import sys
+
+print('Решение задачи №5')
+
+currency_rates(sys.argv[1])
